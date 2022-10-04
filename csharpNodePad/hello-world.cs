448ae4a253558356b52fea54b68ec6f03feef7ae
sultana-hub <cst_sultana2002@yahoo.co.in>
2022-10-04 11:13:21 +0530

    my csharp begiiners code

---
+++ csharpNodePad/hello-world.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace hello-world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is my First C# Program");
+            Console.ReadLine();
+        }
+    }
+}
